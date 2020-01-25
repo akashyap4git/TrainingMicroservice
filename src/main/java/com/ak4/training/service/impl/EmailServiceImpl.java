b94952cbf2b783892ffdb10ee6d7c20130b6dd1c
ambarishk <ambarishk@juniper.net>
2020-01-25 21:06:52 +0530

    Adding Service Annotation for Email

--- src/main/java/com/ak4/training/service/impl/EmailServiceImpl.java
+++ src/main/java/com/ak4/training/service/impl/EmailServiceImpl.java
@@ -3,9 +3,11 @@ package com.ak4.training.service.impl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.stereotype.Service;
 
 import com.ak4.training.service.EMailService;
 
+@Service
 public class EmailServiceImpl implements EMailService {
 	
 	@Autowired
